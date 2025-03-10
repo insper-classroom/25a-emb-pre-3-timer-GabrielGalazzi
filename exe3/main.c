fa7ae4060933d91e34577b17c6dc008f0f33d822
Gabriel <gabrielog@ali.insper.edu.br>
2025-03-10 15:41:14 -0300

    ex3, tentativa 1

--- exe3/main.c
+++ exe3/main.c
@@ -1,4 +1,4 @@
-#include "hardware/gpio.h"
+    #include "hardware/gpio.h"
 #include "pico/stdlib.h"
 #include <stdio.h>
 
@@ -6,16 +6,22 @@ const int BTN_PIN_R = 28;
 const int LED_PIN_R = 4;
 
 volatile int flag_f_r = 0;
+volatile uint64_t time;
 
 void btn_callback(uint gpio, uint32_t events) {
-    if (events == 0x4) { // fall edge
-        if (gpio == BTN_PIN_R)
-            flag_f_r = 1;
 
+    if (events == 0x4) { // fall edge
+        if (gpio == BTN_PIN_R){
+            time = time_us_64();
+        }
     } else if (events == 0x8) { // rise edge
-        if (gpio == BTN_PIN_R)
-            flag_f_r = 0;
+        if (gpio == BTN_PIN_R){
+            if (time_us_64() - time > 500000){
+                flag_f_r = !flag_f_r;
+            }
+        }
     }
+
 }
 
 int main() {
@@ -33,7 +39,11 @@ int main() {
 
     while (true) {
 
-        if (flag_f_r) {
+        if (flag_f_r){
+            gpio_put(LED_PIN_R, 1);
+        }
+        else if (!flag_f_r){
+            gpio_put(LED_PIN_R, 0);
         }
     }
 }
